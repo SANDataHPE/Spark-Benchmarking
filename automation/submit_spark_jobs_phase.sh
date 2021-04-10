df2f8181724d42d783884ea92eb9b050cbca8fad
Varun Rao <vrao@sds.local>
2021-04-09 20:18:21 -0700

    Renaming yaml files

--- automation/submit_spark_jobs_phase.sh
+++ automation/submit_spark_jobs_phase.sh
@@ -2,13 +2,13 @@ namespace=$1
 
 # Submitting join script command
 printf "\n\n\n\nSUBMITTING JOIN SCRIPT\n\n\n\n"
-kubectl apply -f /root/Spark-Benchmarking-main/yamls/spark-submit.yaml -n $namespace &
+kubectl apply -f /root/Spark-Benchmarking-main/yamls/spark-benchmark-join.yaml -n $namespace &
 wait $!
 
 
 # Submitting terragen script
 printf "\n\n\n\nSUBMITTING TERRAGEN SCRIPT\n\n\n\n"
-kubectl apply -f /root/Spark-Benchmarking-main/yamls/terragen.yaml -n $namespace &
+kubectl apply -f /root/Spark-Benchmarking-main/yamls/spark-benchmark-terragen.yaml -n $namespace &
 wait $!
 
 
